0955bc36a542542940d06cb6f10069428250009b
om9022 <omkarchikane607@gmail.com>
2021-06-21 17:49:37 +0530

    Updated: Employee Present or Absent

--- src/main/java/com/empwagecomputation/App.java
+++ src/main/java/com/empwagecomputation/App.java
@@ -1,13 +1,19 @@
 package com.empwagecomputation;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
+	public static final int IS_PRESENT = 1;
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Emp Wage Computation" );
+        double empCheck = Math.floor(Math.random() * 10 )% 2;
+        if (empCheck == IS_PRESENT) 
+        {
+        	System.out.println("Employee is Present");
+        }
+        else
+        {
+        	System.out.println("Employee is Absent");
+        }
     }
 }
