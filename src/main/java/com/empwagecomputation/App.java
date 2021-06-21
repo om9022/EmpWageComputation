166fd183bb55e7fa55b5457879d2728070481a76
om9022 <omkarchikane607@gmail.com>
2021-06-21 18:23:07 +0530

    Updated: Part time Emp Wage

--- src/main/java/com/empwagecomputation/App.java
+++ src/main/java/com/empwagecomputation/App.java
@@ -2,7 +2,9 @@ package com.empwagecomputation;
 
 public class App 
 {
-	public static final int IS_PRESENT = 1;
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int PART_TIME = 4;
 	public static final int FULL_DAY = 8;	
 	public static final int WAGE_PER_HOUR = 20;	
 	
@@ -10,12 +12,18 @@ public class App
     {
         System.out.println( "Welcome to Emp Wage Computation" );
         int dailywage = 0;
-        double empCheck = Math.floor(Math.random() * 10 )% 2;
-        if (empCheck == IS_PRESENT) 
+        double empCheck = Math.floor(Math.random() * 10 )% 3;
+        if (empCheck == IS_FULL_TIME) 
         {
         	dailywage = FULL_DAY * WAGE_PER_HOUR; 
-        	System.out.println("Employee is Present");
+        	System.out.println("Employee is Present for full time");
         	System.out.println("DAily wage is :" + dailywage);
+        }
+        else if ( empCheck == IS_PART_TIME)
+        {
+        	dailywage = PART_TIME * WAGE_PER_HOUR; 
+        	System.out.println("Employee is Present for part time");
+        	System.out.println("DAily wage is :" + dailywage);	
         }
         else
         {
